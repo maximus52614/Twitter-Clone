9a0cd4c6b47d40a666d8e289207c92ca6c580790
Jonnel Joseph <your.jjsuscana@gmail.com>
2017-08-29 10:42:11 +0800

    adding profile picture feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,16 @@ class UsersController < ApplicationController
   def show
   end
 
+  # def create
+  #   @tweet = Tweet.create(content: params[:content], user_id: current_user.id)
+  #     if @tweet.save
+  #       redirect_to user_path(current_user),
+  #         notice: "Tweet success!"
+  #     else
+  #        redirect_to user_path(current_user),
+  #         notice: @tweet.errors.full_messages.to_sentence
+  #     end
+  # end
   # def update
   #   @user = User.update(params[:id])
   # end
